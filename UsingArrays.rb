6794339e6ada7eedf11cd66f31ed266ae71863a5
casper22875 <casper22875@gmail.com>
2015-08-23 21:53:50 -0400

    Update UsingArrays.rb

--- UsingArrays.rb
+++ UsingArrays.rb
@@ -4,3 +4,17 @@ number1 = [1] #This line creates an array with the number 1 inside
 10.times do   #This line tells the next line to do whatever 10 times
 puts number1  #This line displays it onscreen
 end
+#or we can do it this way
+
+puts "List number one 10 times"
+
+numlist = 1
+num = []
+
+10.times do
+   while numlist <= 1
+    num << (numlist)
+    puts numlist
+    break
+    end
+ end
